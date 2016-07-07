62baaef54ac53a312f6a05cd177bc08f288bea89
Daniel Valle <daniel.vallecl@gmail.com>
2016-07-07 15:20:19 -0400

    Finished Step 3

--- ajax.js
+++ ajax.js
@@ -1,12 +1,27 @@
 $(document).on('ready', function () {
 
-  $("button").click(function() {
+  //The Ajax is contained inside the button element//
+
+  $(".button1").click(function() {
     $.ajax({
     url: "http://first-ajax-api.herokuapp.com/",
     method: "GET",
     data: {},
     dataType: "text",
+  }).done( function (responseData) {
+});
   });
+
+  $(".button2").click(function() {
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/ping",
+      method:  "GET",
+      data: {},
+      dataType: "text",
+    }).done (function (responseData) {
+      console.log(responseData);
+      $("#step3456").append(responseData);
   });
+});
 
 });
