a6cab817a171fbee9b5ac38f1c1c29060edf6bf0
Daniel Valle <daniel.vallecl@gmail.com>
2016-07-07 15:55:48 -0400

    Finished Step 7

--- ajax.js
+++ ajax.js
@@ -1,31 +1,42 @@
-$(document).on('ready', function () {
+$(document).on('ready', function() {
 
-  //The Ajax is contained inside the button element//
+    //The Ajax is contained inside the button element//
 
-  $(".button1").click(function() {
-    $.ajax({
-    url: "http://first-ajax-api.herokuapp.com/",
-    method: "GET",
-    data: {},
-    dataType: "text",
-  }).done( function (responseData) {
-});
-  });
+    $(".button1").click(function() {
+        $.ajax({
+            url: "http://first-ajax-api.herokuapp.com/",
+            method: "GET",
+            data: {},
+            dataType: "text",
+        }).done(function(responseData) {});
+    });
 
-  $(".button2").click(function() {
-    $.ajax({
-      url: "http://first-ajax-api.herokuapp.com/pong",
-      method:  "GET",
-      data: {},
-      dataType: "text",
-    }).done (function (responseData) {
-      console.log(responseData);
-      $("#step3456").append(responseData);
-  }).fail (function (responseData) {
-      $("#step3456").append("Sorry, I'll try harder next time!");
-  }).always (function (responseData) {
-      $("step3456").append("Hey the request finished!");
-  });
-});
+    $(".button2").click(function() {
+        $.ajax({
+            url: "http://first-ajax-api.herokuapp.com/pong",
+            method: "GET",
+            data: {},
+            dataType: "text",
+        }).done(function(responseData) {
+            console.log(responseData);
+            $("#step3456").append(responseData);
+        }).fail(function(responseData) {
+            $("#step3456").append("Sorry, I'll try harder next time!");
+        }).always(function(responseData) {
+            $("step3456").append("Hey the request finished!");
+        });
+    });
+
+    $(".button3").click(function() {
+      $.ajax({
+        url: "http://first-ajax-api.herokuapp.com/count",
+        method: "GET",
+        data: {},
+        dataType: "text",
+      }).done(function (responseData) {
+        console.log(responseData);
+        $("#step7").append(responseData);
+      });
+    });
 
 });
